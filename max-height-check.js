e24c042d6fb77f82ff6a4fdbd3949045c3b582e2
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2019-02-01 16:43:25 +0100

    chore: upgraded max-line and max-height functions

--- max-height-check.js
+++ max-height-check.js
@@ -1,13 +1,19 @@
 function maxHeightCheck(orientation = 'portrait') {
   const textBlocks = document.querySelectorAll('[data-max-height]');
   textBlocks.forEach(block => {
-    const computedStyle = window.getComputedStyle(block);
+    const bodyComputedStyle = window.getComputedStyle(document.body);
     const blockHeight = block.scrollHeight;
+    const unit = block.dataset.maxUnit || 'px';
     // Getting the data-max-line attribute value (max number of lines allowed)
     const maxHeightAlt = block.dataset.maxHeightAlt || block.dataset.maxHeight;
-    const maxHeight = (orientation == 'portrait') ? block.dataset.maxHeight : maxHeightAlt;
+    let maxHeight = (orientation == 'portrait') ? block.dataset.maxHeight : maxHeightAlt;
     // Setting the element's max-height 
-    block.style.maxHeight = maxHeight + 'px';
+    block.style.maxHeight = maxHeight + unit;
+
+    if ( unit == 'rem' ) {
+      maxHeight = maxHeight * parseFloat(bodyComputedStyle.fontSize);
+    }
+    
     // Adding an 'overflow' class to an element if it's offset height exceedes the max-line-height
     if ( blockHeight > maxHeight ) {
       block.classList.add('overflow');

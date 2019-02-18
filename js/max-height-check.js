63d650792672aef5ef971f3586fa1debce389fd0
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2019-02-18 15:41:44 +0100

    chore: code cleanup

--- js/max-height-check.js
+++ js/max-height-check.js
@@ -1,17 +1,17 @@
 function maxHeightCheck(variation = 'primary') {
   const textBlocks = document.querySelectorAll('[data-max-height]');
-  
+
   textBlocks.forEach(block => {
     const dynamicCheck = block.dataset.maxHeight == 'dynamic' || block.dataset.maxHeightDynamic == 'true';
     if ( dynamicCheck ) dynamicAssign(block);
-    
-    const cssCheck = block.dataset.maxHeight == 'css';    
+
+    const cssCheck = block.dataset.maxHeight == 'css';
     const bodyComputedStyle = window.getComputedStyle(document.body);
     const blockHeight = block.scrollHeight;
-    const unit = block.dataset.maxUnit || 'px';
+    const unit = block.dataset.maxHeightUnit || 'px';
     const maxHeightAlt = block.dataset.maxHeightAlt || block.dataset.maxHeight;
     let maxHeight = (variation == 'primary') ? block.dataset.maxHeight : maxHeightAlt;    
-    
+
     if ( cssCheck ) {
       const computedBlockStyle = window.getComputedStyle(block);
       maxHeight = parseInt(computedBlockStyle.maxHeight);
@@ -22,15 +22,15 @@ function maxHeightCheck(variation = 'primary') {
       // Recalculating maxHeight in case 'rem' is set as a unit
       if ( unit == 'rem' ) maxHeight = maxHeight * parseFloat(bodyComputedStyle.fontSize);      
     }
-    
+
     // Adding an 'overflow' class to an element if it's offset height exceedes the max-line-height
     ( blockHeight > maxHeight ) ? block.classList.add('overflow') : block.classList.remove('overflow');
   });
 }
 
 function dynamicAssign(element) {
-  const container = element.parentNode;  
-  container.style.overflow = 'hidden';  
+  const container = element.parentNode;
+  container.style.overflow = 'hidden';
   const containerHeight = container.offsetHeight;
   const subtrahendHeight = container.querySelector('.js-subtrahend').offsetHeight || 0;
   const dynamicHeight = containerHeight - subtrahendHeight;

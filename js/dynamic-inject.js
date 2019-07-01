ff24dce255db27df3272a09175a3677331be61e3
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2019-07-01 13:19:03 +0200

    chore: added dynamic-inject funtion

---
+++ js/dynamic-inject.js
@@ -0,0 +1,30 @@
+function injectDynamicContent() {
+    const sources = document.querySelectorAll('.js-inject-src');
+    sources.forEach(source => {
+      const targetID = source.dataset.targetId;
+      const targets = document.querySelectorAll(`[data-target="${targetID}"]`);
+      const repositionCheck = ( source.dataset.isReposition == 'true' );
+
+      if ( repositionCheck ) {
+        const targetInfo = targets[0].getBoundingClientRect();
+        const sourceCss = `position:absolute;left:0;right:0;top:0;bottom:0;opacity:0;width:${targetInfo.width}px;height:${targetInfo.height}px;`
+        source.style.cssText = sourceCss;
+      } else {
+        source.style.display = 'none';
+      }
+
+      targets.forEach(target => {
+        const imageCheck = ( source.dataset.isImage == 'true' );
+
+        if ( imageCheck ) { // Checking if input is regular image input (not using reposition tool)
+          target.src = `${source.innerText}`;
+        } else if ( repositionCheck ) { // Checking if input is image input with reposition tool
+          target.innerHTML = `<div class="reposition-fix">${source.innerHTML}</div>`;
+        } else { // Input is just a regular or rich text input
+          target.innerHTML = source.innerHTML;
+        }
+      });
+    });
+}
+
+injectDynamicContent();

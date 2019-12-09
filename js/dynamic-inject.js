5fec5e7701835d02b82ce4fc29eb823dc1708f57
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2019-12-09 14:12:47 +0100

    fix: emptyCheck fix for image and repositionImage inputs

--- js/dynamic-inject.js
+++ js/dynamic-inject.js
@@ -1,34 +1,36 @@
 function injectDynamicContent() {
-    const sources = document.querySelectorAll('.js-inject-src');
-    sources.forEach(source => {
-      const targetID = source.dataset.targetId;
-      const targets = document.querySelectorAll(`[data-target="${targetID}"]`);
-      const repositionCheck = ( source.dataset.isReposition == 'true' );
+  const sources = document.querySelectorAll('.js-inject-src');
+  sources.forEach(source => {
+    const targetID = source.dataset.targetId;
+    const targets = document.querySelectorAll(`[data-target="${targetID}"]`);
+    const repositionCheck = (source.dataset.isReposition == 'true');
 
-      if ( repositionCheck ) {
-        const targetInfo = targets[0].getBoundingClientRect();
-        const sourceCss = `position:absolute;left:0;top:0;opacity:0;width:${targetInfo.width}px;height:${targetInfo.height}px;`;
-        source.style.cssText = sourceCss;
-      } else {
-        source.style.display = 'none';
+    if (repositionCheck) {
+      const targetInfo = targets[0].getBoundingClientRect();
+      const sourceCss = `position:absolute;left:0;top:0;opacity:0;width:${targetInfo.width}px;height:${targetInfo.height}px;`;
+      source.style.cssText = sourceCss;
+    } else {
+      source.style.display = 'none';
+    }
+
+    targets.forEach(target => {
+      const imageCheck = (source.dataset.isImage === 'true');
+      const preventConditional = target.dataset.preventConditional === 'true';
+      const emptyCheck = !imageCheck && !repositionCheck && source.innerText === '';
+
+      if (imageCheck) { // Checking if input is regular image input (not using reposition tool)
+        target.src = `${source.innerText}`;
+      } else if (repositionCheck) { // Checking if input is image input with reposition tool
+        target.innerHTML = `<div class="reposition-fix">${source.innerHTML}</div>`;
+      } else { // Input is just a regular or rich text input
+        target.innerHTML = source.innerHTML;
       }
 
-      targets.forEach(target => {
-        const imageCheck = ( source.dataset.isImage === 'true' );
-        const preventConditional = target.dataset.preventConditional === 'true';
-        const emptyCheck = source.innerText === '';
-
-        if ( imageCheck ) { // Checking if input is regular image input (not using reposition tool)
-          target.src = `${source.innerText}`;
-        } else if ( repositionCheck ) { // Checking if input is image input with reposition tool
-          target.innerHTML = `<div class="reposition-fix">${source.innerHTML}</div>`;
-        } else { // Input is just a regular or rich text input
-          target.innerHTML = source.innerHTML;
-        }          
-        
-        if ( emptyCheck && !preventConditional ) target.classList.add('u-hide');
-      });
+      if (emptyCheck && !preventConditional) {
+        target.classList.add('u-hide');
+      }
     });
+  });
 }
 
 injectDynamicContent();

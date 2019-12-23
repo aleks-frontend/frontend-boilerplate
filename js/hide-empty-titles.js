a111505df73170fbfb0e07921157d2c75edc7a70
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2019-12-23 13:29:30 +0100

    chore: updated the hideEmptyTitles function to check for individiual empty child nodes

--- js/hide-empty-titles.js
+++ js/hide-empty-titles.js
@@ -7,6 +7,20 @@ function hideEmptyTitles() {
   
   titles.forEach( title => {
     const isEmpty = title.innerText.trim().length == 0;
-    isEmpty ? title.classList.add('u-hide') : title.classList.remove('u-hide');
+    const childNodes = title.querySelectorAll('*');
+    
+    // If all the elements inside the container are empty 'u-hide' is applied to the container
+    isEmpty ? title.classList.add('u-hide') : title.classList.remove('u-hide');       
+
+    // Looping through all the child elements and hiding empty ones
+    // in case there are more elements and not all of them are empty
+    childNodes.forEach(childNode => {
+      const lineBreakCheck = childNode.tagName !== 'BR';
+      const emptyCheck = childNode.innerText.trim().length === 0;
+      
+      if ( emptyCheck && lineBreakCheck ) {
+        childNode.classList.add('u-hide');
+      }
+    })
   });
 }

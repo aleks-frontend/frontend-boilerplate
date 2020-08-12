a5ee08ca24189c30a5b6aeaf6ef197135c7cb9ae
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2020-08-12 14:00:21 +0200

    chore: allImageLoadedCallback added
    Waiting for all the images on page to load and calling the maxHeightCheck and maxLineCheck after that

---
+++ js/all-images-loaded-callback.js
@@ -0,0 +1,26 @@
+function allImagesLoadedCallback() {
+    let imagesLoaded = 0;
+    const images = document.querySelectorAll('img')
+    const totalImages = images.length;
+
+    images.forEach((img) => {
+        const tempImg = document.createElement('img');
+        tempImg.onload = () => {
+            imageLoaded();
+        }
+
+        tempImg.src = img.src;
+    });
+
+    function imageLoaded() {
+        imagesLoaded++;
+        if (imagesLoaded == totalImages) {
+            allImagesLoaded();
+        }
+    }
+
+    function allImagesLoaded() {
+        maxLineCheck();
+        maxHeightCheck();
+    }
+}
